5207c49cb35ccacc8f96b83661a80d224245d283
DESKTOP-3G3KIBU\mcmor <michael.c.mora@gmail.com>
2017-08-29 19:26:16 -0300

    versão inicial.

---
+++ SI-PUCRS-FDS-T178/src/FDSMain.java
@@ -0,0 +1,15 @@
+
+public class FDSMain {
+
+	public static void main(String[] args) {
+		//*******************
+		// Insira, no final deste método, um comentário com 
+		// o seu nome, e um println com o Hello, World, conform
+		// o exemplo abaixo.
+		System.out.println("Hello, World! Comunitario!\n");
+
+		// Michael da Costa Móra
+		System.out.println("Hello, World! Comunitario! Michael\n");
+	}
+
+}
