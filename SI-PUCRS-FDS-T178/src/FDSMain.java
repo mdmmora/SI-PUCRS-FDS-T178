611798f5cb34346e3e3d10a4d12a0901d6dd41c9
DESKTOP-3G3KIBU\mcmor <michael.c.mora@gmail.com>
2017-08-30 10:26:06 -0300

    Atualizado com os últimos updates.

--- SI-PUCRS-FDS-T178/src/FDSMain.java
+++ SI-PUCRS-FDS-T178/src/FDSMain.java
@@ -13,26 +13,22 @@ public class FDSMain {
 		
 		// Wagner Santos
 		System.out.println("Hello, World! Comunitario! Wagner\n");
-<<<<<<< HEAD
-=======
 
 		// Franco Fiorini
 		System.out.println("Hello, World! Comunitario! Franco\n");
 
 		//Kim Schaurich
 		System.out.println("Hello, World! Comunitario! Kim Schaurich\n");
-		
->>>>>>> d59035e1fa71929a7efe21dee86e650f9ab10a56
+
 		// Thiago Roly Gomes
 		System.out.println("Hello, World! Comunitario! Thiago\n");
+		
 		//Catarina Nogueira
 		System.out.println("Hello, World! Comunitario! Catarina\n");
-<<<<<<< HEAD
+
 		// Tamires Domingues
 		System.out.println("Hello, World! Comunitario! Tamires\n");
-=======
 
->>>>>>> d59035e1fa71929a7efe21dee86e650f9ab10a56
 	}
 
 }
